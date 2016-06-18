91c37ae1038639e1a2948c854fc3d5aa19fabd74
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-06-18 22:22:11 +0100

    Changed zookeeper to run with 3 nodes

--- zookeeper/entrypoint.sh
+++ zookeeper/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+mkdir -p /zookeeper/data /zookeeper/wal /zookeeper/log
 mkdir -p /tmp/zookeeper
-./zookeeper/bin/zkServer.sh start
-sleep 2
-./zookeeper/bin/zkCli.sh
+
+if [ -n "$SERVERS" ]; then
+	
+	echo ${MYID:-1} > /tmp/zookeeper/myid
+
+	IFS=\, read -a servers <<<"$SERVERS"
+	for i in "${!servers[@]}"; do 
+		printf "\nserver.%i=%s:2888:3888" "$((1 + $i))" "${servers[$i]}" >> /zookeeper/conf/zoo.cfg
+	done
+fi
+
+echo 'Starting Zookeeper server...'
+./zookeeper/bin/zkServer.sh start-foreground
