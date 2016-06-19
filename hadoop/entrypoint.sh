7db074b72f1844b0baa876efa2e9c2cbb996e8db
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-06-19 17:19:15 +0100

    Changes in the hadoop multinode

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -2,6 +2,12 @@
 
 export JAVA_HOME="${JAVA_HOME:-/usr}"
 
+echo "Starting SSH service..."
+/usr/sbin/sshd-keygen
+/usr/sbin/sshd
+sleep 1
+
+echo "Starting Hadoop..."
 /hadoop/sbin/start-dfs.sh
 /hadoop/sbin/start-yarn.sh
 tail -f /hadoop/logs/*
