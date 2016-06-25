a1251d618496f3abb9698acaa3677614a309fa2a
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-06-25 18:30:55 +0100

    Hadoop image support multiple containers

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -2,6 +2,16 @@
 
 export JAVA_HOME="${JAVA_HOME:-/usr}"
 
+if [ -n "$SLAVES" ]; then 
+	i=1
+	while [ $i -le $SLAVES ]
+	do
+		echo "hadoop-slave$i" >> slaves
+		i=$(( $i + 1 ))
+	done
+	mv slaves /hadoop/etc/hadoop/slaves
+fi
+
 echo "Starting SSH service..."
 /usr/sbin/sshd-keygen
 /usr/sbin/sshd
