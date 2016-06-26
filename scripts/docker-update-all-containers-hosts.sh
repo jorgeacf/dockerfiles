7264d11e63e3395b6f63b636dcce64ca5a382706
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-06-26 16:45:10 +0100

    Added scripts to get container hostnames and update all container hostnames

---
+++ scripts/docker-update-all-containers-hosts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker ps -q | xargs -n 1 docker inspect --format '{{ .NetworkSettings.IPAddress }} {{ .Name }}' | sed 's/ \// /' > /tmp/hosts
+
+awk '{ FS = " " ; print $2 }' /tmp/hosts | sort > /tmp/containers
+
+echo '172.0.0.1 localhost' >> /tmp/hosts
+
+sort /tmp/hosts -o /tmp/hosts
+
+while IFS='' read -r container || [[ -n "$container" ]]; do
+	echo $container
+    docker cp /tmp/hosts $container:hosts
+    docker exec $container /bin/sh -c "cat hosts > /etc/hosts"
+done < /tmp/containers
+
+sudo cp /tmp/hosts /etc/hosts
+
+rm /tmp/hosts
+rm /tmp/containers
