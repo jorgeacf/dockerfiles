e9aa8c7198a1eedbef256c11339153da1f161c0e
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-07-02 13:54:55 +0100

    Added first Kafka multi node dockerfile version

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-/kafka/bin/zookeeper-server-start.sh /kafka/config/zookeeper.properties &
-sleep 10;
+h=`hostname`
+i=$((${#str}-1))
+id=${h:$i:1}
+
+sed -i "s/^broker.id=0.*/broker.id=${id}/" /kafka/config/server.properties
+sed -i "s/^advertised.host.name=.*/advertised.host.name=${h}/" /kafka/config/server.properties
+sed -i "s/^host.name=.*/host.name=${h}/" /kafka/config/server.properties
+
+#/kafka/bin/zookeeper-server-start.sh /kafka/config/zookeeper.properties &
+#sleep 10;
 echo "------- Starting Kafka --------"
 /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
