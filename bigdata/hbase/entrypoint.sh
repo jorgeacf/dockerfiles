7a2a4cb837a81641813bd32f034ba7d6a2b46110
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-12-27 20:40:19 +0000

    HBase changes to run in distributed mode

--- bigdata/hbase/entrypoint.sh
+++ bigdata/hbase/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+sleep 10
+
 export JAVA_HOME="${JAVA_HOME:-/usr}"
+export HBASE_HOME=/hbase
 
 mkdir /hbase/logs
 mkdir -p /tmp/zookeeper
@@ -13,17 +16,28 @@ sleep 1
 if [ "$NODE_TYPE" = "m" ]; then
 	echo 'Running HBase master...';
 	mv /hbase/conf/hbase-site-master.xml /hbase/conf/hbase-site.xml
-	/hbase/bin/start-hbase.sh
+	export HBASE_CONF_DIR=$HBASE_HOME/conf
+	
+  	$HBASE_HOME/bin/hbase-daemons.sh --config "${HBASE_CONF_DIR}" --hosts /hbase/conf/regionservers start regionserver
+
+	$HBASE_HOME/bin/hbase-daemon.sh --config "${HBASE_CONF_DIR}" start master
+
+	#/hbase/bin/hbase-daemon.sh start master
+	#/hbase/bin/start-hbase.sh
 	/hbase/bin/hbase-daemon.sh start rest
 	/hbase/bin/hbase-daemon.sh start thrift
-	/hbase/bin/hbase shell
-	/hbase/bin/stop-hbase.sh
+	#/hbase/bin/hbase shell
+	#/hbase/bin/stop-hbase.sh
+	#sleep 1d
 
 elif [ "$NODE_TYPE" = "s" ]; then
 	echo 'Running HBase slave...';
 	mv /hbase/conf/hbase-site-slave.xml /hbase/conf/hbase-site.xml
-	/hbase/bin/hbase-daemon.sh start regionserver
+	export HBASE_CONF_DIR=$HBASE_HOME/conf
+	#/hbase/bin/start-hbase.sh
+	#/hbase/bin/hbase-daemon.sh start regionserver
 	#/hbase/bin/hbase shell
+	#sleep 1d
 else
 	echo 'Running HBase on standalone mode...'
 	/hbase/bin/hbase zookeeper &>/hbase/logs/zookeeper.log &
