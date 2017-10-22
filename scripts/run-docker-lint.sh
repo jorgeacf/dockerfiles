9d97c40eca1899166121c1b70e6ec501a731856b
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2017-10-22 16:59:35 +0100

    Return status code

--- scripts/run-docker-lint.sh
+++ scripts/run-docker-lint.sh
@@ -2,6 +2,13 @@
 
 dockerfiles=$(find ../ -name "Dockerfile")
 
+status=0;
 for d in $dockerfiles; do
 	dockerlint $d
+	code=$?
+	if [ $code -ne 0 ]; then
+		status=$code;
+	fi
 done
+
+exit $status;
