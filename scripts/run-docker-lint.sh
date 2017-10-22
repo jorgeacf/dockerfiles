a00b871da2d6a32233a62f293bdf5faa843d6f05
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2017-10-22 17:24:35 +0100

    Removed messages

--- scripts/run-docker-lint.sh
+++ scripts/run-docker-lint.sh
@@ -4,13 +4,11 @@ dockerfiles=$(find ../ -name "Dockerfile")
 
 status=0;
 for d in $dockerfiles; do
-	echo "Running dockerlint for [$d]..."
 	dockerlint $d
 	code=$?
 	if [ $code -ne 0 ]; then
 		status=$code;
 	fi
-	echo "-------------------------------"
 done
 
 exit $status;
