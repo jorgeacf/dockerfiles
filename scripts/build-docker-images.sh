0e1b7b19da4a786f0e8607786e7227296a3313fa
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2017-11-12 21:23:48 +0000

    Added build script

---
+++ scripts/build-docker-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a arr=(
+	"build/maven"
+	"build/sbt"
+)
+
+docker images
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+for i in "${arr[@]}"
+do
+   cd "$i"
+   make build push
+   cd -
+done
+
+docker images
