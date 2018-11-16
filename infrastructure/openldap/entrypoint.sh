849040a6a8de8795333baf713adc6a5fdf783b47
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2018-11-16 22:03:02 +0000

    Added Docker openldap

---
+++ infrastructure/openldap/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+LDAP_ROOTPASS=asdasd123
+LDAP_DOMAIN=de-gbi.xyz
+LDAP_ORGANISATION=GBI-DE
+
+
+  cat <<EOF | debconf-set-selections
+slapd slapd/internal/generated_adminpw password ${LDAP_ROOTPASS}
+slapd slapd/internal/adminpw password ${LDAP_ROOTPASS}
+slapd slapd/password2 password ${LDAP_ROOTPASS}
+slapd slapd/password1 password ${LDAP_ROOTPASS}
+slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
+slapd slapd/domain string ${LDAP_DOMAIN}
+slapd shared/organization string ${LDAP_ORGANISATION}
+slapd slapd/backend string HDB
+slapd slapd/purge_database boolean true
+slapd slapd/move_old_database boolean true
+slapd slapd/allow_ldap_v2 boolean false
+slapd slapd/no_configuration boolean false
+slapd slapd/dump_database select when needed
+EOF
+
+dpkg-reconfigure -f noninteractive slapd
+
+/usr/sbin/slapd -h "ldap:///" -u openldap -g openldap -d 0
+
+bash
