e59afc0792b43ebf822c897c4505e0e8dfa1814a
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2019-07-30 00:41:10 +0100

    Trying install admin and other plugins and templates

--- tools/grav/entrypoint.sh
+++ tools/grav/entrypoint.sh
@@ -7,4 +7,17 @@ if ! [ -e index.php -a -e bin/grav ]; then
   echo "Done."
 fi
 
+#pwd;
+
+wget https://getgrav.org/download/skeletons/clean-blog-site/1.4.2/grav-skeleton-clean-blog-v1.4.3.zip
+unzip grav-skeleton-clean-blog-v1.4.3.zip
+cp -r grav-skeleton-clean-blog/. /var/www/html
+chown -R www-data:www-data /var/www/html
+
+bin/gpm selfupgrade -f
+bin/gpm update -f -y
+
+bin/gpm install admin -y
+bin/gpm install git-sync -y
+
 exec "$@"
