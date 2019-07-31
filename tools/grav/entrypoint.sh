6acdaa07067b45414cb57ed7b44c10cc2a096f27
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2019-07-31 22:29:50 +0100

    Updated grav Dockerfile

--- tools/grav/entrypoint.sh
+++ tools/grav/entrypoint.sh
@@ -4,51 +4,36 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
 if ! [ -e index.php -a -e bin/grav ]; then
-  echo "Required files not found in $PWD - Copying from ${SOURCE}..."
-  cp -r "$SOURCE"/. $PWD
-  echo "Modifing user..."
-  chown -R www-data:www-data $PWD
-  echo "Done."
+  	echo "Required files not found in $PWD - Copying from ${SOURCE}..."
+  	cp -r "$SOURCE"/. $PWD
+	SKELETON_VERSION=1.4.2
+	wget https://getgrav.org/download/skeletons/clean-blog-site/${SKELETON_VERSION}/grav-skeleton-clean-blog-v${SKELETON_VERSION}.zip -O /tmp/grav-skeleton.zip
+	unzip /tmp/grav-skeleton.zip -d /tmp
+	cp -r /tmp/grav-skeleton-*/. /var/www/html
+
+  	echo "Modifing user..."
+  	chown -R www-data:www-data $PWD
+  	echo "Done."
 fi
 
-#pwd;
-
-#wget https://getgrav.org/download/skeletons/clean-blog-site/1.4.2/grav-skeleton-clean-blog-v1.4.3.zip
-#unzip grav-skeleton-clean-blog-v1.4.3.zip
-#cp -r grav-skeleton-clean-blog/. /var/www/html
-#chown -R www-data:www-data /var/www/html
-
-#bin/gpm selfupgrade -f
-#bin/gpm update -f -y
-
-#bin/gpm install admin -y
-#bin/gpm install git-sync -y
 
 
 if [[ "$1" = 'grav' ]]; then
-
-	wget https://getgrav.org/download/skeletons/clean-blog-site/1.4.2/grav-skeleton-clean-blog-v1.4.3.zip
-	unzip grav-skeleton-clean-blog-v1.4.3.zip
-	ls -al /var/www/html/
-	rm -r /var/www/html/*
-	ls -al /var/www/html/
-	ls -al .
-	cp -r grav-skeleton-clean-blog/. /var/www/html
 	
-	bin/gpm selfupgrade -f
+	bin/gpm selfupgrade -f -y
 	bin/gpm update -f -y
 
 	bin/gpm install admin -y
 	bin/gpm install git-sync -y
 
 	chown -R www-data:www-data /var/www/html
-	rm -f /etc/nginx/sites-available/defalt
+	rm -f /etc/nginx/sites-available/default
 	rm -f /etc/nginx/sites-enabled/default
 
 	/etc/init.d/nginx start
 	/etc/init.d/php7.3-fpm start
 	echo "Running..."
-	tail -f /dev/null
+	tail -f tail -f /var/log/nginx/access.log
 else
     exec "$@"
 fi
