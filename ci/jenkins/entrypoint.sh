52fa590c08e07208f0a2ce807c156d30545613e5
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2019-09-24 12:30:52 +0100

    Print Jenkins laucher arguments

--- ci/jenkins/entrypoint.sh
+++ ci/jenkins/entrypoint.sh
@@ -8,6 +8,8 @@ touch "${COPY_REFERENCE_FILE_LOG}" || (echo "Can not write to ${COPY_REFERENCE_F
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ -type f -exec bash -c ". /usr/local/bin/jenkins-support; copy_reference_file '{}'" \;
 
+echo "Jenkins Parameters: [$@]"
+
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
   eval "exec java -Duser.home="$JENKINS_HOME" -Dhudson.model.DownloadService.noSignatureCheck=true $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
